005fd7305b50a23844a2fb91e636beaee4cef965
Jose Roberto Quevedo Gabizon <jose.quevedo2011@gmail.com>
2022-03-23 15:38:24 -0400

    fixes :fire:

--- dashboard/components/inputs/Input.tsx
+++ dashboard/components/inputs/Input.tsx
@@ -11,19 +11,18 @@ export interface InputProps
   label: string;
 }
 
-function _Input({ label, className = '', name, ...rest }: InputProps) {
-  return (
-    <label className="block w-full" htmlFor={name}>
-      <span className="text-primary-500 font-semibold">{label}</span>
-      <input
-        className={`mt-1 block w-full text-neutral-800 rounded-md border-neutral-200 shadow-sm focus:border-0 focus:ring focus:ring-primary-100 focus:ring-opacity-50 ${className}`}
-        name={name}
-        {...rest}
-      />
-    </label>
-  );
-}
-
 export const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  (props, ref) => <_Input ref={ref} {...props} />
+  function _Input({ label, className = '', name, ...rest }: InputProps, ref) {
+    return (
+      <label className="block w-full" htmlFor={name}>
+        <span className="text-primary-500 font-semibold">{label}</span>
+        <input
+          {...rest}
+          className={`mt-1 block w-full text-neutral-800 rounded-md border-neutral-200 shadow-sm focus:border-0 focus:ring focus:ring-primary-100 focus:ring-opacity-50 ${className}`}
+          name={name}
+          ref={ref}
+        />
+      </label>
+    );
+  }
 );
