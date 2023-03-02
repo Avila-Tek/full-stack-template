91ee2cde62bd9b8c077145745e3dc36345a8b8dc
Jose Roberto Quevedo Gabizon <jose.quevedo2011@gmail.com>
2023-03-02 11:54:47 -0400

    refactor(api): Updating aws s3 implementation

--- apps/api/src/components/s3/s3.service.ts
+++ apps/api/src/components/s3/s3.service.ts
@@ -1,25 +1,27 @@
-import aws from 'aws-sdk';
-import { S3 } from "@aws-sdk/client-s3";
+import { S3, PutObjectCommand } from '@aws-sdk/client-s3';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import { TSignS3Input } from './s3.dto';
 
 export async function signS3Service(body: TSignS3Input) {
   const bucketName = process?.env?.AWS_BUCKET_NAME ?? '';
-  const spacesEndpoint = new aws.Endpoint('sfo3.digitaloceanspaces.com/prueba');
-  const s3Params = {
+  const s3 = new S3({
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    },
+    endpoint: 'sfo3.digitaloceanspaces.com/prueba',
+    region: process.env.AWS_REGION,
+  });
+  const command = new PutObjectCommand({
     Bucket: bucketName,
-    Key: body?.filename,
-    Expires: 60 * 5,
+    Key: body.filename,
     ContentType: body.filetype,
     ACL: 'public-read',
-  };
-  const s3 = new S3({
-    apiVersion: '2021-07-26',
-    endpoint: spacesEndpoint,
-    region: process.env.AWS_REGION,
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   });
-  const signedRequest = s3.getSignedUrl('putObject', s3Params);
+  const oneHrInSec = 60 * 60;
+  const signedRequest = await getSignedUrl(s3, command, {
+    expiresIn: oneHrInSec,
+  });
   const url = `https://avilatek.sfo3.digitaloceanspaces.com/prueba/${body.filename}`;
   return {
     signedRequest,
